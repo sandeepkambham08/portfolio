769e39c1216239d0ed6f5f567ac014ab8c635603
sandeep <sandeepkambham08@gmail.com>
2022-03-17 21:51:12 -0400

    remove dead code

--- src/App.js
+++ src/App.js
@@ -21,46 +21,9 @@ import resume from "./media/Sandeep_Kambham_Resume.pdf";
 
 import SandeepAnimation from "./Sandeep_animation/Sandeep_animation.js";
 
-// let scrollDirection = 0; // To detect scroll direction
-let scrollPercentage; // Amount the view is scrolled
-
-// const url = "https://jsonplaceholder.typicode.com/posts";
-// fetch(url,'POST')
-// .then( data=>{
-//   console.log(data);
-//   return data.json()})
-// .then(res=>{
-//   console.log("Hello")
-//   console.log(res)
-// });
-
+let scrollPercentage; 
 class App extends Component {
   render() {
-    //   window.addEventListener("scroll", (event) => {
-    //     let height = window.innerHeight;
-    //     let scroll = window.scrollY;
-    //     scrollPercentage = scroll/height;
-    //     if(scrollPercentage>1){                           // Don't show header in first screen
-    //       scrollDirection= scrollPercentage/scrollDirection;
-    //       if(scrollDirection<1){
-    //       // console.log(scrollDirection, ' , ', scrollPercentage)
-    //       document.querySelector('header').classList.add('App-header');
-    //       document.querySelector('header').classList.remove('hide');
-    //       }
-    //       if(scrollDirection>1){
-    //         // console.log(scrollDirection, ' , ', scrollPercentage)
-    //         document.querySelector('header').classList.remove('App-header');
-    //         document.querySelector('header').classList.add('hide');
-    //         }
-    //     }
-    //                          // Don't show header in first screen
-    //     if(scrollPercentage<=1){
-    //       document.querySelector('header').classList.remove('App-header');
-    //       document.querySelector('header').classList.add('hide');
-    //     }
-
-    //     scrollDirection=scrollPercentage; // saving scrollpercentage for next calculation
-    // });
     const sandeep_logo = document.querySelectorAll("#Sandeep-name path");
     console.log(sandeep_logo);
     for (let i = 0; i < sandeep_logo.length; i++) {
@@ -68,20 +31,10 @@ class App extends Component {
     }
     return (
       <div className="App">
-        {/* <header className='hide'>
-       <p>Sandeep portfolio</p>
-      </header> */}
-        {/* <div >
-        <div id="left"></div>
-        <div id="right"></div>
-        <div id="top"></div>
-        <div id="bottom"></div>
-      </div> */}
+       
         <Navigator />
         <div className="App-body">
           <div className="Initial-page section" id="About">
-            {/* {window.addEventListener('scroll', this.checkHeader)} */}
-            {/* <div></div> */}
             <div className="Sandeep-logo-div">
               <img src={js_logo} alt="js_logo" className="js-logo" />
               <img src={logo} alt="Sandeep_logo" className="Sandeep-logo" />
@@ -145,24 +98,3 @@ class App extends Component {
 }
 
 export default App;
-
-// const targets = document.querySelectorAll("div.section")
-// console.log(targets);
-
-// // window.addEventListener('DOMContentLoaded', () => {
-//   const lazyload = target => {
-
-//     const io = new IntersectionObserver((entries,observer)=>{
-//       entries.forEach(entry=>{
-//         console.log('Loving ');
-//         if(entry.isIntersecting){
-//           console.log(entry);
-//         }
-//         observer.disconnect();
-//       })
-//     });
-
-//     io.observe(target)
-
-//   }
-//   targets.forEach(lazyload);
