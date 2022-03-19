ab28b60853df03cd6fd80e19d5d1e9a90165bb74
sandeep <sandeepkambham08@gmail.com>
2022-03-19 14:50:15 -0400

    make job item a component

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -4,76 +4,51 @@ import Amazon_logo from '../media/Amazon-logo.png';
 import Techm_logo from '../media/Techm-logo.png';
 import Wepeoples_logo from '../media/Wepeoples_logo.png';
 
+import Job_item from './Job_Item/Job_item';
+
 const Experience = () => {
   return (
     <div className="Experience section" id="Experience">
       <br></br>
       <h1 className="Experience-title font-bold text-3xl py-4">Experience</h1>
       {/* Drakkar Digital */}
-      <div className="w-3/4 lg:w-1/2 mx-auto grid grid-flow-row lg:grid-flow-col  lg:gap-4 pt-8">
-        <div className="grid content-center row-span-1">
-          <img
-            className="w-24 lg:w-32"
-            src={Drakkar_logo}
-            alt="Drakkar digital logo"
-          />
-          <p className="py-2 text-gray-500 text-left ">Mar 2021 - Present</p>
-        </div>
-        <div>
-          <div className="text-left font-bold text-xl py-2">
-            Front-end Developer
-          </div>
-          <div className="job-description">
-            Worked on several projects which allowed me to try several cutting
+      <Job_item
+        logo={Drakkar_logo}
+        logoAltText="Drakkar digital logo"
+        jobPeriod="Mar 2021 - Present"
+        jobTitle="Front-end Developer"
+        jobDescription="Worked on several projects which allowed me to try several cutting
             edge technologies. Some of the things I did include, migrating a
             huge project to Typescript, development of a multiple gaming site,
             creating reusable web components and integrate them into React,
             development of a high traffic central portal where users under
-            several websites can manage and access their profile.
-          </div>
-          <p className="text-left">Location: Montreal, Canada</p>
-        </div>
-      </div>
+            several websites can manage and access their profile."
+        jobLocation="Montreal, Canada"
+      />
       {/* We peoples */}
-      <div className="w-3/4 lg:w-1/2 mx-auto grid grid-flow-row lg:grid-flow-col lg:gap-4 pt-8">
-        <div className="grid content-center">
-          <img
-            className="w-24 lg:w-32"
-            src={Wepeoples_logo}
-            alt="Wepeoples logo"
-          />
-          <p class="text-left lg:text-center font-bold text-green-700">
-            We Peoples
-          </p>
-          <p className="py-2 text-gray-500 text-left ">Aug 2018 - May 2019</p>
-        </div>
-        <div>
-          <div className="text-left font-bold text-xl py-2">
-            Front end Engineer
-          </div>
-          <div className="job-description">
-            Developed multiple UI components which include posts, comments and
+      <Job_item
+        logo={Wepeoples_logo}
+        logoAltText="Wepeoples logo"
+        company="wepeoples"
+        jobPeriod="Aug 2018 - May 2019"
+        jobTitle="Front end Engineer"
+        jobDescription="Developed multiple UI components which include posts, comments and
             replies to an issue created. Created page navigation using react
             router and user authentication using google OAuth. Worked closely
             with the backend and UX teams for developing responsive user
             interfaces. Actively involved in the team Stand-ups, retrospective,
-            code reviews and created user stories on JIRA.
-          </div>
-          <p className="text-left">Location: Remote</p>
-        </div>
-      </div>
+            code reviews and created user stories on JIRA."
+        jobLocation="Remote"
+      />
+
       {/* Amazon */}
-      <div className="w-3/4 lg:w-1/2 mx-auto grid grid-flow-row lg:grid-flow-col lg:gap-4 pt-8">
-        <div className="grid content-center">
-          <img className="w-24 lg:w-32" src={Amazon_logo} alt="Amazon logo" />
-          <p className="py-2 text-gray-500 text-left ">May 2017 - Aug 2018</p>
-        </div>
-        <div>
-          <div className="text-left font-bold text-xl py-2">
-            DevOps Engineer
-          </div>
-          <div className="job-description">
-            I have created user scripts using JavaScript to improve efficiency.
+
+      <Job_item
+        logo={Amazon_logo}
+        logoAltText="Amazon logo"
+        jobPeriod="May 2017 - Aug 2018"
+        jobTitle="DevOps Engineer"
+        jobDescription="I have created user scripts using JavaScript to improve efficiency.
             I have developed an add-on form on the website to perform multiple
             manual updates on a single click, this reduced the action time for
             working on a task by 25%. I was as a key person in migrating an
@@ -82,32 +57,24 @@ const Experience = () => {
             changes happening in the companies transportation network using test
             deployments into the systems. I worked with various teams across the
             globe and suggested about the errors in their proposed change
-            requests.
-          </div>
-          <p className="text-left">Location: Hyderabad</p>
-        </div>
-      </div>
+            requests."
+        jobLocation="Hyderabad"
+      />
+
       {/* Tech Mahindra */}
-      <div className="w-3/4 lg:w-1/2 mx-auto grid grid-flow-row lg:grid-flow-col lg:gap-4 pt-8">
-        <div className="grid content-center">
-          <img className="w-24 lg:w-32" src={Techm_logo} alt="Techm logo" />
-          <p className="py-2 text-gray-500 text-left ">Aug 2016 - Apr 2017</p>
-        </div>
-        <div>
-          <div className="text-left font-bold text-xl py-2">
-            Software Engineer
-          </div>
-          <div className="job-description">
-            Right after my B.Tech, I joined as a software engineer in Tech
+      <Job_item
+        logo={Techm_logo}
+        logoAltText="Techm logo"
+        jobPeriod="Aug 2016 - Apr 2017"
+        jobTitle="Software Engineer"
+        jobDescription="Right after my B.Tech, I joined as a software engineer in Tech
             Mahindra. Tech Mahindra is a technology company based in India with
             it's clients around the world. I worked in transactions team which
             involved working with database systems and visualization tools like
             Tableau. I have gained a good experience in working with relational
-            DB's and using Tableau.
-          </div>
-          <p className="text-left">Location: Hyderabad</p>
-        </div>
-      </div>
+            DB's and using Tableau."
+        jobLocation="Hyderabad"
+      />
     </div>
   );
 };
--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -11,26 +11,26 @@ const Skills = () => {
       <div className="divide grid gap-2">
         <div className="grid gap-2">
           <div>
-            <h2 class="font-bold text-xl">Core</h2>
+            <h2 className="font-bold text-xl">Core</h2>
             <p>JavaScript</p>
             <p>HTML5</p>
             <p>CSS</p>
             <p>TypeScript</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">Frameworks</h2>
+            <h2 className="font-bold text-xl">Frameworks</h2>
             <p>React</p>
             <p>React-Native (basic)</p>
             <p>GatsbyJS</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">Libraries</h2>
+            <h2 className="font-bold text-xl">Libraries</h2>
             <p>Redux</p>
             <p>Jquery</p>
             <p>Bootstrap</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">Elixir stack</h2>
+            <h2 className="font-bold text-xl">Elixir stack</h2>
             <p>Elixir</p>
             <p>Phoenix</p>
             <p>Liveview</p>
@@ -39,30 +39,30 @@ const Skills = () => {
         </div>
         <div className="grid gap-2">
           <div>
-            <h2 class="font-bold text-xl">Related</h2>
+            <h2 className="font-bold text-xl">Related</h2>
             <p>GraphQL</p>
             <p>AJAX</p>
             <p>React-router</p>
             <p>REST API's</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">Database</h2>
+            <h2 className="font-bold text-xl">Database</h2>
             <p>MySQL</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">AWS</h2>
+            <h2 className="font-bold text-xl">AWS</h2>
             <p>EC2</p>
             <p>S3</p>
             <p>lambda</p>
             <p>Route53</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">Tools</h2>
+            <h2 className="font-bold text-xl">Tools</h2>
             <p>WinSCP</p>
             <p>putty</p>
           </div>
           <div>
-            <h2 class="font-bold text-xl">Others</h2>
+            <h2 className="font-bold text-xl">Others</h2>
             <p>webRTC, GitHub, npm, material UI</p>
           </div>
         </div>
